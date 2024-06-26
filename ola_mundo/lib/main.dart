cb35079b9b38e3bf71c1e58aa87cd3c7ff5cdac0
Leonardo Villani <leonardo.villani@fatec.sp.gov.br>
2024-06-26 18:58:31 -0300

    Aula 05: Usando containers

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -4,17 +4,31 @@ main() => runApp(
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text('Meu App'),
-            backgroundColor: Colors.orange,
+            title: Text('Usando Containers'),
+            backgroundColor: Colors.blueAccent,
           ),
-          body: Row(
-            children: [
-              Expanded(flex: 2, child: Icon(Icons.shopping_cart)),
-              Expanded(flex: 2, child: Icon(Icons.vpn_lock)),
-              Expanded(flex: 2, child: Icon(Icons.restaurant)),
-              Expanded(flex: 10, child: Icon(Icons.tablet_mac)),
-              Expanded(flex: 1, child: Icon(Icons.cloud)),
-            ],
+          body: Container(
+            width: double.infinity,
+            margin: EdgeInsets.all(10.0),
+            padding: EdgeInsets.all(20.0),
+            decoration: BoxDecoration(
+              color: Color(0xFF8CE1F8),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(5, 5),
+                  blurRadius: 5,
+                )
+              ],
+              border: Border.all(
+                color: Color(0xFF51A3D6),
+                width: 2,
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              )
+            ),
+            child: Text(
+                'Este é um texto muito, muito grande, grande demais para caber em uma tela normal em uma única linha!'),
           ),
           backgroundColor: Colors.blueGrey,
           floatingActionButton: FloatingActionButton(
