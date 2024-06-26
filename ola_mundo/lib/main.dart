888c1132be752b83a317f3da46533b67b90231f8
Leonardo Villani <leonardo.villani@fatec.sp.gov.br>
2024-06-26 18:30:45 -0300

    Aula 04: Usando widgets de colunas e linhas

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
 
 main() => runApp(
-  MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Meu App'),
-        backgroundColor: Colors.orange,
+      MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Meu App'),
+            backgroundColor: Colors.orange,
+          ),
+          body: Row(
+            children: [
+              Expanded(flex: 2, child: Icon(Icons.shopping_cart)),
+              Expanded(flex: 2, child: Icon(Icons.vpn_lock)),
+              Expanded(flex: 2, child: Icon(Icons.restaurant)),
+              Expanded(flex: 10, child: Icon(Icons.tablet_mac)),
+              Expanded(flex: 1, child: Icon(Icons.cloud)),
+            ],
+          ),
+          backgroundColor: Colors.blueGrey,
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            child: Icon(Icons.add),
+          ),
+        ),
       ),
-      body: Text('Olá mundo!'),
-      backgroundColor: Colors.blueGrey,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-      ),
-    ),
-  ),
-);
+    );
