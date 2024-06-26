307a1bb5030d1b146ed4a755f0a10796135a0714
Leonardo Villani <leonardo.villani@fatec.sp.gov.br>
2024-06-26 16:17:29 -0300

    Aula 03: Usando widgets de layout

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 
-main() => runApp(Center(
-      child: Text(
-        'Olá mundo!',
-        textDirection: TextDirection.ltr,
+main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Meu App'),
+        backgroundColor: Colors.orange,
       ),
-    ));
+      body: Text('Olá mundo!'),
+      backgroundColor: Colors.blueGrey,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+    ),
+  ),
+);
