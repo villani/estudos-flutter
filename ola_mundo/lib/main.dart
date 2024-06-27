db0fbf84aa236c3f58fddb17eca80025d2032471
Leonardo Villani <leonardo.villani@fatec.sp.gov.br>
2024-06-27 09:00:03 -0300

    Aula 07: Usando widgtes de interface

--- ola_mundo/lib/main.dart
+++ ola_mundo/lib/main.dart
@@ -4,39 +4,19 @@ main() => runApp(
       MaterialApp(
         home: Scaffold(
           appBar: AppBar(
-            title: Text('Usando Containers'),
+            title: const Text('Widgets de interface'),
             backgroundColor: Colors.blueAccent,
           ),
-          body: Stack(
-            children: [
-              Container(
-                width: 300,
-                height: 300,
-                color: Colors.green,
+          body: const Center(
+            child: Text(
+              'Flutter é simples',
+              style: TextStyle(
+                fontSize: 30,
+                color: Colors.orange,
+                fontWeight: FontWeight.bold,
+                decoration: TextDecoration.underline,
               ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: Container(
-                  width: 200,
-                  height: 200,
-                  color: Colors.yellow,
-                ),
-              ),
-              Positioned(
-                right: 50,
-                top: 200,
-                child: Container(
-                  width: 120,
-                  height: 120,
-                  color: Colors.blue,
-                ),
-              ),
-            ],
-          ),
-          backgroundColor: Colors.blueGrey,
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            child: Icon(Icons.add),
+            ),
           ),
         ),
       ),
